0432ac804f666ef144d7be8b361144a4fa313de7
Mohammad hifzan <misterj435@gmail.com>
2024-09-25 03:17:26 +0000

    chore:removed some unnecessary users

--- number_guess.sql
+++ number_guess.sql
@@ -72,11 +72,32 @@ INSERT INTO public.users VALUES ('user_1727229039414', 0, 0);
 INSERT INTO public.users VALUES ('user_1727229039413', 0, 0);
 INSERT INTO public.users VALUES ('user_1727229302831', 0, 0);
 INSERT INTO public.users VALUES ('user_1727229302830', 0, 0);
+INSERT INTO public.users VALUES ('user_1727232258020', 2, 476);
+INSERT INTO public.users VALUES ('user_1727232258021', 5, 70);
 INSERT INTO public.users VALUES ('user_1727229820472', 2, 0);
 INSERT INTO public.users VALUES ('user_1727229820473', 5, 0);
-INSERT INTO public.users VALUES ('hifzan', 3, 6);
+INSERT INTO public.users VALUES ('user_1727232334241', 2, 911);
 INSERT INTO public.users VALUES ('user_1727229942583', 2, 0);
+INSERT INTO public.users VALUES ('user_1727232334242', 5, 107);
 INSERT INTO public.users VALUES ('user_1727229942584', 5, 0);
+INSERT INTO public.users VALUES ('user_1727230661717', 2, 0);
+INSERT INTO public.users VALUES ('user_1727230661718', 5, 0);
+INSERT INTO public.users VALUES ('user_1727232377043', 2, 105);
+INSERT INTO public.users VALUES ('hifzan', 5, 6);
+INSERT INTO public.users VALUES ('user_1727232377044', 5, 64);
+INSERT INTO public.users VALUES ('rohan', 4, 10);
+INSERT INTO public.users VALUES ('user_1727231265477', 2, 746);
+INSERT INTO public.users VALUES ('user_1727231265478', 5, 13);
+INSERT INTO public.users VALUES ('user_1727232412613', 2, 980);
+INSERT INTO public.users VALUES ('user_1727232140787', 2, 802);
+INSERT INTO public.users VALUES ('user_1727232412614', 5, 248);
+INSERT INTO public.users VALUES ('user_1727232140788', 4, 72);
+INSERT INTO public.users VALUES ('user_1727232180906', 2, 250);
+INSERT INTO public.users VALUES ('user_1727232180907', 5, 138);
+INSERT INTO public.users VALUES ('user_1727232452356', 2, 921);
+INSERT INTO public.users VALUES ('user_1727232452357', 5, 80);
+INSERT INTO public.users VALUES ('user_1727232502300', 2, 62);
+INSERT INTO public.users VALUES ('user_1727232502301', 5, 144);
 
 
 --
